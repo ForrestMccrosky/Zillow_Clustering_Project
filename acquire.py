f68f0e1dafd2783c35c1fcee2d118a943ada1352
Your Name <forrest.mccrosky@gmail.com>
2021-06-22 14:23:51 -0500

    adding acquire phase of pipeline

--- acquire.py
+++ acquire.py
@@ -12,6 +12,7 @@ warnings.filterwarnings("ignore")
 ####################### Function File For Acquire Phase ###########################
 
 
+
 ####################### Connect To SQL database Function ##########################
 
 def sql_connect(db, user=user, host=host, password=password):
@@ -20,6 +21,7 @@ def sql_connect(db, user=user, host=host, password=password):
     Using private information from my env.py file.
     '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
 
 ####################### Acquire Zillow Database Function ##########################
 
@@ -45,15 +47,83 @@ def acquire_zillow():
     left join storytype using(storytypeid)
     left join typeconstructiontype using(typeconstructiontypeid)
 
-    where latitude is not null
-
-    and longitude is not null
+    where latitude is not null and longitude is not null
 
     '''
     
     df = pd.read_sql(sql_query, sql_connect('zillow'))
     
+    
+    ## filtering for properties that single residential properties
+    df = df[df['propertylandusetypeid'] == 261]
+    
+    ##getting rid of duplicate columns
+    df= df.loc[:, ~df.columns.duplicated()]
+    
     ## drop duplicate parcelids keeping the latest transaction from 2017
     df = df.sort_values('transactiondate').drop_duplicates('parcelid',keep='last') 
     
     return df 
+
+
+####################### Quick Acquire Summary Function ##########################
+
+def quick_sum(df):
+    '''
+    This function prints out a quick summary of the acquired data looking at the following    
+    duplicates
+    and Transposed numerical statistics
+    data types
+    '''
+    ## printing out the shape of the data
+    print('Dataframe Shape (rows, columns):')
+    print(df.shape)
+    print('-----------------------------------------------')
+    print('')
+    
+    ## looking at datatypes
+    print('Datatypes of Columns:')
+    print(df.info())
+    print('-----------------------------------------------')
+    print('')
+    
+    ## Looking at Transposed Numerical Statistics
+    print('Numerical Statistics:')
+    print(df.describe())
+    
+    
+####################### Discrete Variable Value Counts Function ##########################
+
+def object_values(df):
+    '''
+    This function takes a look at the value counts of all our columns that have the data type 
+    object
+    '''
+    
+    ## creating a list of object columns from the dataframe using list comprehension
+    obj_cols = [col for col in df.columns if (df[col].dtype == "object")]
+    
+    ## looping through the list to print out the list of object columns value counts
+    for col in obj_cols:
+        
+        print(col)
+        print(df[col].value_counts())
+        print('-----------------------------------------------')
+        print()
+        
+####################### Continuous Variable Distributions ##########################
+
+def cont_hist(df):
+    '''
+    This function takes a look at all the distributions of the continuous variables
+    by utilizing histograms
+    '''
+    
+    ## making a list of continuous variable columns
+    cont_cols = [col for col in df.columns if (df[col].dtype == 'int64') | (df[col].dtype =='float64')]
+    
+    ## looping through the list to plot histograms for each column
+    for col in cont_cols:
+        plt.hist(df[col])
+        plt.title(f"{col} distribution")
+        plt.show()
